14fab958c50c6bcc97dab6cb4540b1f1e0143849
Patrick Gray <patrickgray@Patricks-MacBook-Pro.local>
2018-06-29 15:10:59 -0600

    targetting function works

--- js/app.js
+++ js/app.js
@@ -11,8 +11,8 @@ const gameBoard =[ 	[0,0,0,0],
 
 for (let i = 0; i < gameBoard.length; i++){
 	let row = gameBoard[i];
-	$('#alien-gameboard').append(`<div class="alien-row-${i} game-row"></div>`)
-	$('#player-gameboard').append(`<div class="player-row-${i} game-row"></div>`)
+	$('#alien-gameboard').append(`<div class="alien-row-${i} alien-row game-row"></div>`)
+	$('#player-gameboard').append(`<div class="player-row-${i} player-row game-row"></div>`)
 	for(let x = 0; x < row.length; x++){
 		const square = row[x];
 		$(`.alien-row-${i}`).append(`<div class="square alien-square-${x}-${i} alien-square"></div>`)
@@ -39,6 +39,7 @@ class Gun {
 	}
 	attackgun(target){
 		target.integrity = target.integrity-this.damage
+		console.log(target)
 	}
 }
 
@@ -51,8 +52,31 @@ const playerHull = new Hull(10)
 const playerGun = new Gun(1, 3)
 
 //players can target other cards to do damage
-alienGun.attackhull(playerHull)
-console.log(playerHull)
+//alienGun.attackhull(playerHull) check check
+
+//hard code the hull on to the array
+
+$(".alien-square-1-2").addClass("hull-class");
+$(".alien-square-2-2").addClass("gun-class");
+
+$(".player-square-2-0").addClass("hull-class");
+$(".player-square-1-0").addClass("gun-class");
+
+
+//player can do damage to the computer with the power of jQuery
+
+	$(".alien-square").click(function(e){
+		console.log("alien-square");
+		if($(e.currentTarget).hasClass("hull-class")){
+			console.log("classy")
+			playerGun.attackhull(alienHull)
+		} else if($(e.currentTarget).hasClass("gun-class")){
+			console.log('shooter')
+			playerGun.attackgun(alienGun)
+		}		
+	})	
+
+//.hasclass checks to se if it has a class
 
 
 //player has hand of 2 guns and 1 hull of two blocks
