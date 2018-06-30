4fa12b59f0f2ae5582f79a35b56d34e7906cc68d
Patrick Gray <patrickgray@Patricks-MacBook-Pro.local>
2018-06-29 19:27:16 -0600

    added construction round and wokred on startgame function

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
-console.log("Engines are go!")
 
 //hard code game board and cards on board
 
 	//create array for player and computer and append to html
+
+
+
+
+
+
 const gameBoard =[ 	[0,0,0,0],
 					[0,0,0,0],
 					[0,0,0,0],
@@ -56,15 +61,86 @@ const playerGun = new Gun(1, 3)
 
 //hard code the hull on to the array
 
+const player ={
+	hullPartsInHand : [],
+	hullPartsInPlay : [],
+	hullPartsDestroyed : [],
+	gunPartsInHand : [],
+	gunPartsInPlay : [],
+	gunPartsDestroyed : [],
+	addToShip: function(){
+		$(".player-square").click(function(e){
+		console.log("player-square");
+		}) 
+	},
+	playHull: function(){
+		$(".player-square").click(function(e){
+		$(e.currentTarget).addClass("hull-class");
+		const moveHull = hullPartsInHand.splice([0],1);
+		hullPartsInPlay.push(moveHull); 
+		})
+	}
+}
+
+// }
+	//playerLife : total life for all player hull parts in play
+	//}
+
+	// }
+
+const alien ={
+	hullPartsInHand : [],
+	hullPartsInPlay : [],
+	hullPartsDestroyed : [],
+	gunPartsInHand : [],
+	gunPartsInPlay : [], 
+	partsDestroyed : [],
+	//alienlife 
+}
+
+
+const game = {
+	dealParts : function(){
+		player.hullPartsInHand.push(playerHull);
+		player.gunPartsInHand.push(playerGun);
+		alien.hullPartsInHand.push(alienHull);
+		alien.gunPartsInHand.push(alienGun);
+	},
+	firstHull : function(){
+		player.playHull();		
+	},
+	constructionRound : function(){
+		if ((player.hullPartsInHand.length>0) || (player.gunPartsInHand>0)){
+			console.log("cards to play!")
+			$('#action-button').text("Constuction").attr("id","construction")
+			$('#construction').click(function(){
+				player.addToShip()
+			})
+
+		} else {
+			console.log("move to battle")
+		}
+	}, 
+}
+
+const startGame = () => {
+	game.dealParts();
+	game.constructionRound();
+}
+
+startGame();
+
+
 $(".alien-square-1-2").addClass("hull-class");
 $(".alien-square-2-2").addClass("gun-class");
 
-$(".player-square-2-0").addClass("hull-class");
+//$(".player-square-2-0").addClass("hull-class");
 $(".player-square-1-0").addClass("gun-class");
 
 
 //player can do damage to the computer with the power of jQuery
-
+//.hasclass checks to se if it has a class
+const enemyTargeting = () => {
 	$(".alien-square").click(function(e){
 		console.log("alien-square");
 		if($(e.currentTarget).hasClass("hull-class")){
@@ -73,10 +149,19 @@ $(".player-square-1-0").addClass("gun-class");
 		} else if($(e.currentTarget).hasClass("gun-class")){
 			console.log('shooter')
 			playerGun.attackgun(alienGun)
-		}		
+		} 		
 	})	
+}
 
-//.hasclass checks to se if it has a class
+// const playerTargeting = () => {
+// 	$(".player-square").click(function(e){
+// 		console.log("player-square");
+// 		if($(e.currentTarget).hasClass("gun-class")){
+// 			alert("arming weapons -- target the enemy when ready")
+// 			enemyTargeting()
+// 		}  		
+// 	})
+// }
 
 
 //player has hand of 2 guns and 1 hull of two blocks
